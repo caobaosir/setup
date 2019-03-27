2dfc7a07bc27cd9edeb0ed5595397fabd8a359f8
caobaosir <lqabc6666@gmail.com>
2019-03-28 02:27:16 +0800

    3.28

--- dockerfile
+++ dockerfile
@@ -1,26 +1,12 @@
-FROM alpine:edge as builder
+FROM alpine:edge
 
-RUN apk add wget
-RUN wget -O go.sh https://raw.githubusercontent.com/caobaosir/setup/master/ins_wg_alpine.sh
-RUN chmod +x go.sh
-RUN ./go.sh
+RUN wget -O /tmp/go.sh https://raw.githubusercontent.com/caobaosir/setup/master/ins_wg_alpine.sh \
+            chmod +rwx /tmp/go.sh \
+            /tmp/go.sh
+
+LABEL maintainer "alpine:edge install wireguard wg(x) for docker by caobaosir"
 
 
+ENTRYPOINT ["/usr/src/run/run.sh"]
 
-LABEL maintainer "alpine install wg for docker by caobaosir"
-
-COPY --from=builder /usr/bin/v2ray/v2ray /usr/bin/v2ray/
-COPY --from=builder /usr/bin/v2ray/v2ctl /usr/bin/v2ray/
-COPY --from=builder /usr/bin/v2ray/geoip.dat /usr/bin/v2ray/
-COPY --from=builder /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/
-COPY config.json /etc/v2ray/config.json
-
-RUN set -ex && \
-    apk --no-cache add ca-certificates && \
-    mkdir /var/log/v2ray/ &&\
-    chmod +x /usr/bin/v2ray/v2ctl && \
-    chmod +x /usr/bin/v2ray/v2ray
-
-ENV PATH /usr/bin/v2ray:$PATH
-
-CMD ["v2ray", "-config=/etc/v2ray/config.json"]
+CMD []
--- ins_wg_alpine.sh
+++ ins_wg_alpine.sh
@@ -1,77 +1,15 @@
-#目前wg需要edge/testing，国内使用清华源
-
 echo 'http://mirrors.tuna.tsinghua.edu.cn/alpine/edge/testing' >> /etc/apk/repositories
 echo 'http://mirrors.tuna.tsinghua.edu.cn/alpine/edge/main' >> /etc/apk/repositories
-
-
-apk update   #更新源索引文件
-apk add curl sudo
-
-apk add linux-virt      #linux-vanilla linux-vanilla-dev
-apk add wireguard-virt  #wireguard-vanilla
-apk add wireguard-tools bash openrc iptables
-
-#服务端配置文件（单/多用户）
-mkdir /etc/wireguard
-cat > /etc/wireguard/wg0.conf <<EOF
-[Interface]
-PrivateKey = cFGAuatqg8RHfes4P+iphOGMp7XUNjq16K6ksfuuJ3Y=
-Address = 10.0.0.1/24
-ListenPort = 53988
-DNS = 8.8.8.8
-MTU = 1420
-
-[Peer]
-PublicKey = lqofQOo5TU2Ji1vXWQ49xOp7B0PCTJQqYBApSZDryiI=
-AllowedIPs = 10.0.0.2/32
-
+apk update
+apk add linux-virt wireguard-virt wireguard-tools iptables bash
+mkdir /usr/src
+mkdir /usr/src/run
+cd /usr/src/run
+cat >run.sh<<EOF
+#!/bin/bash
+while true
+do
+sleep 1
+done
 EOF
-
-#服务器网卡配置====/etc/network/interfaces ======================
-sudo chmod 777 /etc/network/interfaces
-sudo cat >> /etc/network/interfaces <<EOF
-auto wg0
-iface wg0 inet static
-    address 10.0.0.1
-    netmask 255.255.255.0
-    pre-up ip link add dev wg0 type wireguard
-    pre-up wg setconf wg0 /etc/wireguard/wg0.conf
-    post-down ip link delete dev wg0
-EOF
-
-
-
-#接口启停
-#ifup wg0
-#ifdown wg0
-
-#启用wg
-sudo wg-quick up wg0
-sudo wg
-
-#设置开机自启
-sudo cat > /etc/local.d/wireguardss.start <<EOF
-wg-quick down wg0
-wg
-wg-quick up wg0
-wg
-EOF
-sudo chmod 777 /etc/local.d/wireguardss.start
-sudo rc-update add local
-
-#配置iptables
-
-sudo rc-update add iptables
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -F
-sudo service iptables save
-sudo service iptables restart
-
-#启用ip4路由
-sudo chmod 777 /proc/sys/net/ipv4/ip_forward
-sudo echo 1 > /proc/sys/net/ipv4/ip_forward
-sudo chmod 777 /etc/sysctl.conf
-sudo echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
-sudo sysctl -p /etc/sysctl.conf
+chmod +x run.sh
