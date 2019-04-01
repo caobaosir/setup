ae8dba7d19d5300378f8922d5adb12dea2b4cb11
caobaosir <48466472+caobaosir@users.noreply.github.com>
2019-04-01 14:52:18 +0800

    add wg4

---
+++ ins_wg_alpine4.sh
@@ -0,0 +1,22 @@
+echo 'http://mirrors.tuna.tsinghua.edu.cn/alpine/edge/testing' >> /etc/apk/repositories
+echo 'http://mirrors.tuna.tsinghua.edu.cn/alpine/edge/main' >> /etc/apk/repositories
+apk update
+apk add linux-virt wireguard-virt wireguard-tools iptables bash
+mkdir /usr/src
+mkdir /usr/src/run
+cd /usr/src/run
+
+cat >run.sh<<EOF
+#!/bin/sh
+wg
+while true
+do
+sleep 1
+done
+EOF
+chmod +x run.sh
+
+
+
+
+#end
